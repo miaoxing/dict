198047a69a14e9e74780b099a9b12af66cad61bc
twinh <twinhuang@qq.com>
2025-01-17 16:43:33 +0800

    refactor(internal): 生成元数据到模型类中，删除 Trait

--- src/Service/DictItemModel.php
+++ src/Service/DictItemModel.php
@@ -2,7 +2,6 @@
 
 namespace Miaoxing\Dict\Service;
 
-use Miaoxing\Dict\Metadata\DictItemTrait;
 use Miaoxing\Plugin\BaseModel;
 use Miaoxing\Plugin\Model\ModelTrait;
 use Miaoxing\Plugin\Model\ReqQueryTrait;
@@ -10,11 +9,36 @@ use Miaoxing\Plugin\Model\SnowflakeTrait;
 use Wei\Model\SoftDeleteTrait;
 
 /**
+ * @property string|null $id
+ * @property string $dictId
+ * @property string $name 名称
+ * @property string $value 值
+ * @property int $sort
+ * @property bool $isEnabled 是否启用
+ * @property string $remark 备注
+ * @property string|null $createdAt
+ * @property string|null $updatedAt
+ * @property string $createdBy
+ * @property string $updatedBy
+ * @property string|null $deletedAt
+ * @property string $deletedBy
  * @property DictModel $dict
+ * @property string|null $id
+ * @property string $dictId
+ * @property string $name 名称
+ * @property string $value 值
+ * @property int $sort
+ * @property bool $isEnabled 是否启用
+ * @property string $remark 备注
+ * @property string|null $createdAt
+ * @property string|null $updatedAt
+ * @property string $createdBy
+ * @property string $updatedBy
+ * @property string|null $deletedAt
+ * @property string $deletedBy
  */
 class DictItemModel extends BaseModel
 {
-    use DictItemTrait;
     use ModelTrait;
     use ReqQueryTrait;
     use SnowflakeTrait;
--- src/Service/DictModel.php
+++ src/Service/DictModel.php
@@ -2,16 +2,28 @@
 
 namespace Miaoxing\Dict\Service;
 
-use Miaoxing\Dict\Metadata\DictTrait;
 use Miaoxing\Plugin\BaseModel;
 use Miaoxing\Plugin\Model\ModelTrait;
 use Miaoxing\Plugin\Model\ReqQueryTrait;
 use Miaoxing\Plugin\Model\SnowflakeTrait;
 use Wei\Model\SoftDeleteTrait;
 
+/**
+ * @property string|null $id
+ * @property string $name 名称
+ * @property string $code 标识
+ * @property bool $isBuiltIn 是否内置
+ * @property string $remark 备注
+ * @property string|null $createdAt
+ * @property string|null $updatedAt
+ * @property string $createdBy
+ * @property string $updatedBy
+ * @property string|null $deletedAt
+ * @property string $deletedBy
+ * @property DictItemModel $items
+ */
 class DictModel extends BaseModel
 {
-    use DictTrait;
     use ModelTrait;
     use ReqQueryTrait;
     use SnowflakeTrait;
